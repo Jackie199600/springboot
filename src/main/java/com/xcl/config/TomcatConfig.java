c324a502a26ff3ad242b669492279fcd57b75689
Jackie199600 <xcl199600@126.com>
2019-11-27 10:01:26 +0800

    添加配置

---
+++ src/main/java/com/xcl/config/TomcatConfig.java
@@ -0,0 +1,26 @@
+package com.xcl.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jackie  Chan
+ * Date: 2019/11/27
+ * Time: 9:32
+ */
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public TomcatServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers((Connector connector) -> {
+            connector.setProperty("relaxedPathChars", "\"<>[\\]^`{|}");
+            connector.setProperty("relaxedQueryChars", "\"<>[\\]^`{|}");
+        });
+        return factory;
+    }
+}
